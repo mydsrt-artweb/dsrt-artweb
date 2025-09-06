1add8fe2e458dc6fd8e2414ed1c955bb92c975d8
mydsrt-artweb <my.project944@gmail.com>
2025-09-06 23:33:39 +0700

    Create Header.tsx

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="p-5 text-center border-b-2 border-teal-400 bg-black/20 z-20">
+      <h1 className="m-0 text-teal-400 text-xl md:text-2xl font-bold">
+        DSRT — Digital Smart Revise Technology
+      </h1>
+    </header>
+  );
+}
