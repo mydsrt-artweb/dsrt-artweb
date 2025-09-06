925e5b843c58c1d94e7b091c08033109e6061fe6
mydsrt-artweb <my.project944@gmail.com>
2025-09-06 23:32:42 +0700

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#001f27] p-4 text-center text-teal-400 border-t-2 border-teal-400">
+      &copy; 2025 DSRT — All rights reserved.
+    </footer>
+  );
+}
