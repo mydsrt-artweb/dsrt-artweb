9a95a45243ef1cec194ec8e22698f390ebb41fd1
mydsrt-artweb <my.project944@gmail.com>
2025-09-07 10:29:55 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,19 @@ import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import RainBackground from "@/components/RainBackground";
 
-export default function Home() {
+export default function Page() {
   return (
-    <>
+    <main className="relative min-h-screen flex flex-col">
       <RainBackground />
+
       <Header />
-      <main className="flex-1 relative z-10 flex flex-col items-center px-5 py-12 lg:px-20 lg:py-20">
+
+      <div className="flex-1">
         <Hero />
         <Features />
-      </main>
+      </div>
+
       <Footer />
-    </>
+    </main>
   );
 }
