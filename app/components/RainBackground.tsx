9f5106ef36f371b13ee9e936d32c9948ffabb2a3
mydsrt-artweb <my.project944@gmail.com>
2025-09-07 10:27:38 +0700

    Update RainBackground.tsx

--- app/components/RainBackground.tsx
+++ app/components/RainBackground.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function RainBackground() {
+  useEffect(() => {
+    console.log("RainBackground effect loaded");
+  }, []);
+
   return (
-    <div className="fixed inset-0 pointer-events-none overflow-hidden z-0">
-      <span className="r left-[15%] h-[140vh] animate-fall delay-0"></span>
-      <span className="r left-[38%] h-[120vh] animate-fall delay-[1.5s]"></span>
-      <span className="r left-[61%] h-[160vh] animate-fall delay-[3s]"></span>
-      <span className="r left-[84%] h-[130vh] animate-fall delay-[4.5s]"></span>
+    <div className="absolute inset-0 -z-10 bg-gradient-to-b from-blue-200 to-blue-400">
+      {/* Dummy background */}
     </div>
   );
 }
